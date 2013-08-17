3d49ad087576ef104384ed2f9e26d887bc5a3d64
liisa <liisa.sailaranta@aalto.fi>
2013-08-17 13:20:03 +0300

    Loading bar works. html tags cleaned out from results

--- gen/com/mainapp/mynoppaapp/R.java
+++ gen/com/mainapp/mynoppaapp/R.java
@@ -15,18 +15,17 @@ public final class R {
         public static final int rec=0x7f020001;
     }
     public static final class id {
-        public static final int Row1Text=0x7f070003;
-        public static final int Row2Text=0x7f070005;
-        public static final int TableLayout01=0x7f070001;
-        public static final int edit_message=0x7f07000a;
-        public static final int header=0x7f070000;
-        public static final int listview=0x7f070009;
-        public static final int menu_settings=0x7f07000b;
-        public static final int progressBar1=0x7f070008;
-        public static final int tableRow1=0x7f070002;
-        public static final int tableRow2=0x7f070004;
-        public static final int tableRow3=0x7f070006;
-        public static final int tableRow4=0x7f070007;
+        public static final int ScrollView01=0x7f070000;
+        public static final int TableLayout01=0x7f070003;
+        public static final int edit_message=0x7f070009;
+        public static final int header=0x7f070002;
+        public static final int instruction=0x7f070008;
+        public static final int listview=0x7f070007;
+        public static final int menu_settings=0x7f07000a;
+        public static final int progressBar1=0x7f070006;
+        public static final int rel=0x7f070001;
+        public static final int tableRow1=0x7f070004;
+        public static final int tableRow2=0x7f070005;
     }
     public static final class layout {
         public static final int activity_display_course_details=0x7f030000;
@@ -40,11 +39,12 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int button_send=0x7f040003;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_display_course_details=0x7f040005;
-        public static final int title_activity_display_search_results=0x7f040004;
+        public static final int button_send=0x7f040004;
+        public static final int instruction=0x7f040001;
+        public static final int menu_settings=0x7f040003;
+        public static final int title_activity_display_course_details=0x7f040006;
+        public static final int title_activity_display_search_results=0x7f040005;
+        public static final int type_course_name=0x7f040002;
     }
     public static final class style {
         /** 
--- src/com/mainapp/mynoppaapp/DisplayCourseDetails.java
+++ src/com/mainapp/mynoppaapp/DisplayCourseDetails.java
@@ -4,20 +4,37 @@ import java.net.URL;
 import java.util.Iterator;
 import java.util.Set;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+import android.graphics.Point;
 import android.inputmethodservice.Keyboard.Row;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.text.Html;
+import android.util.Log;
+import android.view.Display;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -36,48 +53,17 @@ public class DisplayCourseDetails extends Activity {
 		course = (Course) intent
 				.getSerializableExtra(DisplaySearchResultsActivity.EXTRA_MESSAGE);
 
-		TextView row1 = (TextView) findViewById(R.id.Row1Text);
-		row1.setText(course.getName());
+		setRow(course.getName());
+		setRow(course.getCourse_id());
+		setRow(course.getCourse_url());
+		setRow(course.getNoppa_language());
 
-		TableLayout table = (TableLayout) findViewById(R.id.TableLayout01);
-		// create a new TableRow
-		TableRow row = new TableRow(DisplayCourseDetails.this);
-		// create a new TextView for showing xml data
-		TextView t = new TextView(DisplayCourseDetails.this);
-		// set the text to "text xx"
-		t.setText(course.getCourse_id());
-		row.addView(t);
-		table.addView(row, new TableLayout.LayoutParams(
-				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-
-		// create a new TableRow
-		TableRow row2 = new TableRow(DisplayCourseDetails.this);
-		// create a new TextView for showing xml data
-		TextView t2 = new TextView(DisplayCourseDetails.this);
-		// set the text to "text xx"
-		t2.setText(course.getCourse_url());
-		row2.addView(t2);
-		// // add the TableRow to the TableLayout
-		table.addView(row2, new TableLayout.LayoutParams(
-				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-
-		// create a new TableRow
-		TableRow row3 = new TableRow(DisplayCourseDetails.this);
-		// create a new TextView for showing xml data
-		TextView t3 = new TextView(DisplayCourseDetails.this);
-		// set the text to "text xx"
-		t3.setText(course.getNoppa_language());
-		row3.addView(t3);
-		// // add the TableRow to the TableLayout
-		table.addView(row3, new TableLayout.LayoutParams(
-				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 		final String url = host + "courses/" + course.getCourse_id()
 				+ "/overview?" + key;
 		new DownloadFilesTask().execute(url);
 
 	}
 
-	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -85,27 +71,42 @@ public class DisplayCourseDetails extends Activity {
 		return true;
 	}
 
-	private class DownloadFilesTask extends AsyncTask<String, Integer, Long> {
-		private void setRow(String text){
-			TableLayout table = (TableLayout) findViewById(R.id.TableLayout01);
-			TableRow row4 = new TableRow(
-					DisplayCourseDetails.this);
-			// create a new TextView for showing xml
-			// data
-			TextView t4 = new TextView(
-					DisplayCourseDetails.this);
-			// set the text to "text xx"
-			t4.setText(text);
-			row4.addView(t4);
-			// // add the TableRow to the
-			// TableLayout
-			table.addView(row4,
-					new TableLayout.LayoutParams(
-							LayoutParams.WRAP_CONTENT,
-							LayoutParams.WRAP_CONTENT));
-		}
-		protected Long doInBackground(final String... url) {
+	@SuppressLint("NewApi")
+	private void setRow(String text) {
+		TableLayout table = (TableLayout) findViewById(R.id.TableLayout01);
+		TableRow row = new TableRow(DisplayCourseDetails.this);
+		// create a new TextView for showing xml
+		TextView t = new TextView(DisplayCourseDetails.this);
+
+		// check whether given text has some html tags..
+		// Log.e("MYAPP", "exception", e);
+		text = Html.fromHtml(text).toString();
+
 		
+		t.setText(text);
+		t.setTextIsSelectable(true);
+		t.setTextSize(22);
+
+		TableRow.LayoutParams l = new TableRow.LayoutParams(
+				TableLayout.LayoutParams.WRAP_CONTENT,
+				TableLayout.LayoutParams.WRAP_CONTENT, 1f);
+		t.setLayoutParams(l);
+		row.addView(t);
+		// // add the TableRow to the
+		// TableLayout
+
+		table.addView(row, new TableLayout.LayoutParams(
+				TableLayout.LayoutParams.MATCH_PARENT,
+				TableLayout.LayoutParams.MATCH_PARENT));
+
+	}
+
+	private class DownloadFilesTask extends AsyncTask<String, Integer, Long> {
+
+		protected Long doInBackground(final String... url) {
+			ProgressBar mProgress = (ProgressBar) findViewById(R.id.progressBar1);
+			mProgress.setVisibility(View.VISIBLE);
+
 			try {
 				final String r = MainActivity.CONNECTOR.getResults(url[0]);
 				JSONTokener jsontokener = new JSONTokener(r);
@@ -117,11 +118,13 @@ public class DisplayCourseDetails extends Activity {
 						synchronized (course) {
 							try {
 								Iterator it = obj.keys();
-								while (it.hasNext())
-								{
+								while (it.hasNext()) {
+									// TODO consider saving this values
+									// somewhere for later use
 									setRow((String) obj.get((String) it.next()));
+
 								}
-								
+
 							} catch (JSONException e) {
 								// TODO Auto-generated catch block
 								e.printStackTrace();
@@ -136,25 +139,15 @@ public class DisplayCourseDetails extends Activity {
 				e.printStackTrace();
 			}
 
-			
 			return null;
 		}
 
 		protected void onProgressUpdate(Integer... progress) {
-			
+
 		}
 
 		protected void onPostExecute(Long result) {
-			TableLayout table = (TableLayout) findViewById(R.id.TableLayout01);
-			TableRow row5 = new TableRow(DisplayCourseDetails.this);
-			// create a new TextView for showing xml data
-			TextView t5 = new TextView(DisplayCourseDetails.this);
-			// set the text to "text xx"
-			t5.setText("dummy");
-			row5.addView(t5);
-			// // add the TableRow to the TableLayout
-			table.addView(row5, new TableLayout.LayoutParams(
-					LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+
 			ProgressBar mProgress = (ProgressBar) findViewById(R.id.progressBar1);
 			mProgress.setVisibility(View.GONE);
 		}
